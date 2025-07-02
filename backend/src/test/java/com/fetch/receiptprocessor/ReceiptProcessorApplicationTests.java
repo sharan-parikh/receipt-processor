6e2b4f0abeec789da1e8b0149c85376d3cebf60e
Sharan Parikh <parikhsharan6@gmail.com>
2025-07-02 11:28:50 -0400

    Added active profile to ReceiptProcessorApplicationTests

--- backend/src/test/java/com/fetch/receiptprocessor/ReceiptProcessorApplicationTests.java
+++ backend/src/test/java/com/fetch/receiptprocessor/ReceiptProcessorApplicationTests.java
@@ -2,8 +2,10 @@ package com.fetch.receiptprocessor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Use the test profile to avoid security configurations
 class ReceiptProcessorApplicationTests {
 
 	@Test
